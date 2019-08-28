68a2591ae05c6f2444fbb4cf2c080b0c89329cc6
Aryeh Katz <in@akatz.io>
2019-08-28 19:20:32 +0300

    show use of getWhitelistableTransaction

--- src/app.js
+++ src/app.js
@@ -1,48 +1,51 @@
 "use strict";
 (async function(KinSdk, KeystoreProvider) {
-	const SECRET_KEY = 'my secret key';
-	let keyStoreProvider = new KeystoreProvider(KinSdk, SECRET_KEY);
-	let kinClient = new KinSdk.KinClient(
-		KinSdk.Environment.Testnet,
-		keyStoreProvider
-	);
+  const SECRET_KEY = "my secret key";
+  let keyStoreProvider = new KeystoreProvider(KinSdk, SECRET_KEY);
+  let kinClient = new KinSdk.KinClient(
+    KinSdk.Environment.Testnet,
+    keyStoreProvider
+  );
 
-	let accounts = await kinClient.kinAccounts;
-	switch (accounts.length){
-		case 2: 
-			break;
-		case 0:
-			await keyStoreProvider.addKeyPair();
-		case 1:
-			await keyStoreProvider.addKeyPair();
-		default:
-			accounts = await kinClient.kinAccounts;
-			break;
-	}
-	
-	const transactionId = await kinClient.friendbot({
-		address: accounts[0].publicAddress,
-		amount: 1000,
-	});
-	
-	const secondTransactionId = await kinClient.friendbot({
-		address: accounts[1].publicAddress,
-		amount: 1000,
-	});
+  let accounts = await kinClient.kinAccounts;
+  switch (accounts.length) {
+    case 2:
+      break;
+    case 0:
+      await keyStoreProvider.addKeyPair();
+    case 1:
+      await keyStoreProvider.addKeyPair();
+    default:
+      accounts = await kinClient.kinAccounts;
+      break;
+  }
 
-	const transaction = await accounts[0].buildTransaction({
-		address: accounts[1].publicAddress,
-		amount: 1,
-		fee: 100,
-		memoText: "Send some kin",
-	});
+  const transactionId = await kinClient.friendbot({
+    address: accounts[0].publicAddress,
+    amount: 1000
+  });
 
-	await accounts[0].submitTransaction(transaction.toString());
+  const secondTransactionId = await kinClient.friendbot({
+    address: accounts[1].publicAddress,
+    amount: 1000
+  });
 
-	const senderBalance = await accounts[0].getBalance();
-	const receiverBalance = await accounts[1].getBalance();
+  const transaction = await accounts[0].buildTransaction({
+    address: accounts[1].publicAddress,
+    amount: 1,
+    fee: 100,
+    memoText: "Send some kin"
+  });
 
-	console.log(senderBalance);
-	console.log(receiverBalance);
-	
+  await accounts[0].submitTransaction(transaction.toString());
+  const senderBalance = await accounts[0].getBalance();
+  const receiverBalance = await accounts[1].getBalance();
+
+  console.log(senderBalance);
+  console.log(receiverBalance);
+
+  const whitelistabletransaction = await accounts[0].getWhitelistableTransaction(
+    transaction.toString()
+  );
+  console.log(whitelistabletransaction);
 })(window.KinSdk, window.KeystoreProvider);
